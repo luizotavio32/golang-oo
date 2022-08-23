65baa5e344e8cb0078980920614d013ad1107e78
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-23 19:42:09 -0300

    feat: struct definition

--- hello-world.go
+++ hello-world.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+    titular       string
+    numeroAgencia int
+    numeroConta   int
+    saldo         float64
+}
+
 func main() {
-    fmt.Println("hello world")
+    conta := ContaCorrente{"Luiz", 10, 10, 500000.0}
+    fmt.Println(conta)
 }
