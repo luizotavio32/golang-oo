fedad783d6b160228e2ff9dcae3ad872aa66fde4
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-23 20:07:12 -0300

    feat: pointer struct

--- main.go
+++ main.go
@@ -12,4 +12,11 @@ type ContaCorrente struct {
 func main() {
     conta := ContaCorrente{"Luiz", 10, 10, 500000.0}
     fmt.Println(conta)
+
+    var conta2 *ContaCorrente
+    conta2 = new(ContaCorrente)
+    conta2.titular = "Luiz"
+    fmt.Println(*conta2)
+
+    fmt.Println("Memory address conta2: ", &conta2)
 }
