26927662c9db97afa2a7cdaf8082d4f7aba26e7b
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-23 21:09:29 -0300

    feat: add withdraw method

--- main.go
+++ main.go
@@ -9,14 +9,30 @@ type ContaCorrente struct {
     saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+
 func main() {
     conta := ContaCorrente{"Luiz", 10, 10, 500000.0}
     fmt.Println(conta)
 
-    var conta2 *ContaCorrente
-    conta2 = new(ContaCorrente)
-    conta2.titular = "Luiz"
-    fmt.Println(*conta2)
+    fmt.Println(conta.saldo)
 
-    fmt.Println("Memory address conta2: ", &conta2)
+	fmt.Println(conta.Sacar(400))
+	fmt.Println(conta.saldo)
+
+    //var conta2 *ContaCorrente
+    //conta2 = new(ContaCorrente)
+    //conta2.titular = "Luiz"
+    //fmt.Println(*conta2)
+
+    //fmt.Println("Memory address conta2: ", &conta2)
 }
